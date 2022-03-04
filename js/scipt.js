546806921a7a217ff7184ed65851d74b84d51c1e
YuliaPol <62880025+YuliaPol@users.noreply.github.com>
2022-03-04 17:53:56 +0200

    first commit

---
+++ js/scipt.js
@@ -0,0 +1,44 @@
+
+jQuery(function ($) {
+    $(document).ready(function () {
+        $('.page-wrap').on('input', '.diapason-answer .input-range', function(e){
+            setDiapasonValue(this);
+            $('.form-wrapper').find('.btn-submit').addClass('active')
+            $('.form-wrapper').find('.comment-answer').addClass('shown');
+        });
+        //set new diapsson value
+        function setDiapasonValue(input){
+            var value = $(input).val();
+            var max = $(input).attr('max');
+            var min = $(input).attr('min');
+            var range = max - min;
+            var relvalue = value - min;
+            var percent = (100/range)*relvalue;
+            var parents = $(input).parents('.diapason-answer');
+            var paddleft = (34*percent)/100;
+            parents.find('.label').css('left', 'calc(' + percent + '% - ' + paddleft + 'px)');
+            parents.find('.value').css('left', 'calc(' + percent + '% - ' + paddleft + 'px)');
+            parents.find('.value').html(value);
+            parents.find('.input-box .bar-filled').css('width', percent + '%');
+            parents.find('.label').css('background-position', percent + '%');
+            if(value === min){
+                parents.find('.top-label').find('span:nth-child(1)').addClass('hidden');
+            } else {
+                parents.find('.top-label').find('span:nth-child(1)').removeClass('hidden');
+            }
+
+            if(value === max){
+                parents.find('.top-label').find('span:nth-child(2)').addClass('hidden');
+            } else {
+                parents.find('.top-label').find('span:nth-child(2)').removeClass('hidden');
+            }
+
+            if(value > min) {
+                console.log(parents.find('.label'));
+                parents.find('.label').addClass('active');
+            } else {
+                parents.find('.label').removeClass('active');
+            }
+        };
+    });
+});
